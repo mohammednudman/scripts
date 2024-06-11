3fad94fc6459327a6445e63afaf48e51bc5d48fb
mohammed <mohammednudman@gmail.com>
2024-06-11 18:03:40 +0530

    Done

---
+++ jsonConverter/normal.py
@@ -0,0 +1,70 @@
+import re
+import json
+
+default_values = {
+    "RootOrderID": "",
+    "ClordID": "",
+    "OrigClordID": "",
+    "Instance": "LCETEST01",
+    "SourceSysCode": "",
+    "TraderID": "",
+    "Region": "",
+    "TradeDate": "",
+    "Trader_Location": "",
+    "Booking_ID": "",
+    "DeskID": "",
+    "BookingLegalEntityID": "",
+    "OrderQty": 0,
+    "Instrument": "",
+    "Price": 0.0,
+    "Side": "",
+    "Instrument_Type": "",
+    "Product": "",
+    "OrdType": "",
+    "Capacity": "",
+    "TimeInForce": "",
+    "FlowType": "",
+    "Country": "",
+    "Currency": "",
+    "RLChecks": "", 
+    "DOUPDATE": "",
+    "IS_CBFM": 0,
+    "Order_Action": "",
+    "CheckContext": {
+        "Checks": []
+    },
+    "CheckOverrides": []
+}
+
+def parse_request(request_str):
+    order_event = default_values.copy()
+    match = re.findall(r'(\w+): "([^"]+)"', request_str)
+    for key, value in match:
+        order_event[key.strip()] = value.strip()
+    return {"NewOrder": order_event}
+
+def parse_file(file_path):
+    order_events = []
+    
+    with open(file_path, 'r') as file:
+        for line in file:
+            requests = line.split("order_event_uuid")
+            for request_str in requests[1:]:
+                order_events.append(parse_request("order_event_uuid" + request_str))
+    
+    return order_events
+
+def create_request(order_events):
+    request = {
+        "CheckRequest": {
+            "OrderEventInfos": order_events
+        }
+    }
+    
+    return json.dumps(request, indent=2)
+
+
+file_path = "test.txt"
+order_events = parse_file(file_path)
+request_json = create_request(order_events)
+print(request_json)
