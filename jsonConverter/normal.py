de8a7f8bc498f5c7545c40f088a12aa74e425ddd
Mohammed Nudman Raza Shaikh <97384468+mohammednudman@users.noreply.github.com>
2024-06-12 13:03:23 +0530

    Update normal.py

--- jsonConverter/normal.py
+++ jsonConverter/normal.py
@@ -38,10 +38,12 @@ default_values = {
 
 def parse_request(request_str):
     order_event = default_values.copy()
-    match = re.findall(r'(\w+): "([^"]+)"', request_str)
+    match = re.findall(r'(\w+):\s*"([^"]+)"', request_str)
     for key, value in match:
         if key in order_event:
             order_event[key.strip()] = value.strip()
+        elif key == "RISKPARAMS":
+            order_event["CheckContext"]["Checks"] = [val for val in value.split('|') if val]
     return {"NewOrder": order_event}
 
 def parse_file(file_path):
